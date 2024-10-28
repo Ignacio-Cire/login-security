5c2024133950d3de3a75635de3d12fad44d3ef94
Gabriel Ignacio Cire <gabriel.cire@est.fi.uncoma.edu.ar>
2024-10-28 19:53:46 -0300

    pagina-segura-sin-terminar

--- login/views/action/verificarLogin.php
+++ login/views/action/verificarLogin.php
@@ -36,28 +36,33 @@ if ($datos) {
     // Crear una instancia de Usuario
     $usuario = new Usuario(); 
 
-        if ($baseDatos->Iniciar()) {
+    if ($baseDatos->Iniciar()) {
         // Llama a tu método para obtener el usuario por email
-        $usuarioData = $usuario->obtenerPorEmail($baseDatos, $email); // 
+        $usuarioData = $usuario->obtenerPorEmail($baseDatos, $email);
 
         if ($usuarioData) {
             // Verifica la contraseña
-            if (password_verify($password, $usuarioData['password'])) { // Asegúrate de que el password esté en el array
+            if (password_verify($password, $usuarioData['password'])) {
                 // Guarda la información del usuario en la sesión
-                $_SESSION['usuario'] = $usuarioData['nombreUsuario']; // O cualquier otro campo que quieras guardar
-                echo 'Inicio de sesión exitoso. Bienvenido, ' . $_SESSION['usuario'] . '.';
+                $_SESSION['usuario'] = $usuarioData['nombreUsuario'];
+                
+                // Redirige a paginaSegura.php si el inicio de sesión es exitoso
+                header('Location: ../views/paginaSegura.php');
+                exit();
             } else {
                 echo 'La contraseña es incorrecta. Inténtalo de nuevo.';
+                header('Location: ../Vista/login.php'); // Redirige de vuelta a login si la contraseña es incorrecta
+                exit();
             }
         } else {
             echo 'No existe un usuario con ese email. Por favor, verifica e intenta nuevamente.';
+            header('Location: ../view/login.php'); // Redirige de vuelta a login si el email es incorrecto
+            exit();
         }
     } else {
-        echo 'Error en la conexión a la base de datos.'; // Mensaje de error
+        echo 'Error en la conexión a la base de datos.';
     }
 
     exit();
 }
-
 ?>
-
